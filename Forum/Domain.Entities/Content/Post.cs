012f7a6f24ee013cb50dd4a5cf19a786a855e759
tearth <Tearth@users.noreply.github.com>
2017-08-15 22:34:51 +0200

    Add SetContent method to Post

--- Forum/Domain.Entities/Content/Post.cs
+++ Forum/Domain.Entities/Content/Post.cs
@@ -10,25 +10,21 @@ namespace Domain.Entities.Content
     {
         public DateTime CreateTime { get; private set; }
         public DateTime ModifyTime { get; private set; }
-
-        private string _content;
-        public string Content
-        {
-            get { return _content; }
-            set
-            {
-                ModifyTime = DateTime.Now;
-                _content = value;
-            }
-        }
+        private string Content { get; private set; }
 
         public virtual Topic Topic { get; set; }
 
         public Post(string content)
         {
-            _content = content;
+            Content = content;
 
             CreateTime = DateTime.Now;
+            ModifyTime = DateTime.Now;
+        }
+
+        public void SetContent(string content)
+        {
+            Content = content;
             ModifyTime = DateTime.Now;
         }
     }
