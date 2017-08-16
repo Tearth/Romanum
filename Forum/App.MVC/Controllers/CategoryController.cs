1fdbea7c7d920fb2995eff0f7b6166f519806610
tearth <Tearth@users.noreply.github.com>
2017-08-16 23:16:50 +0200

    Add category view

---
+++ Forum/App.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public CategoryController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
