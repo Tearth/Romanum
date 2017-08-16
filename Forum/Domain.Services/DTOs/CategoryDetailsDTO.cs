ff94e4cb6f6783c09c2c01394bdb06f9288e37a8
tearth <Tearth@users.noreply.github.com>
2017-08-16 16:39:50 +0200

    Add DTOs for section and category details

---
+++ Forum/Domain.Services/DTOs/CategoryDetailsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.DTOs
+{
+    public class CategoryDetailsDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TopicsCount { get; set; }
+        public int PostsCount { get; set; }
+        public DateTime LastPostTime { get; set; }
+    }
+}
