7a1080d4bf9210c02043c3b78a40a43788f93d53
tearth <Tearth@users.noreply.github.com>
2017-08-17 12:39:10 +0200

    Add fake DbSet factory

---
+++ Forum/Domain.Services.Tests/SectionServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Domain.Services.Tests
+{
+    public class SectionServiceTests
+    {
+        [Fact]
+        public void GetAllSetionsWithCategories_ValidData_ValidResult()
+        {
+
+        }
+    }
+}
