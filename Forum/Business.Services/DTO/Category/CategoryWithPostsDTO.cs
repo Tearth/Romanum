8e426a05905d30d991ab1c48538da49b7e4956a8
tearth <Tearth@users.noreply.github.com>
2017-08-27 18:41:28 +0200

    Add DTO for category view Add CategoryWithPostsDTO and PostDetailsDTO

---
+++ Forum/Business.Services/DTO/Category/CategoryWithPostsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.DTO.Category
+{
+    public class CategoryWithPostsDTO
+    {
+        public String Name { get; set; }
+        public String Alias { get; set; }
+        public IEnumerable<PostDetailsDTO> Posts { get; set; }
+    }
+}
---
+++ Forum/Business.Services/DTO/Category/PostDetailsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.DTO.Category
+{
+    public class PostDetailsDTO
+    {
+        public String Name { get; set; }
+        public String Alias { get; set; }
+        public String AuthorName { get; set; }
+        public int UniqueNumber { get; set; }
+        public DateTime CreateTime { get; set; }
+        public String LastPostAuthorName { get; set; }
+        public DateTime LastPostTime { get; set; }
+    }
+}
