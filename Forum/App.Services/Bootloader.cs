f3a07decc4270ec6975ea6e3271eb7749124fb72
tearth <Tearth@users.noreply.github.com>
2017-08-28 12:57:58 +0200

    Fix lack of assemblies when loading Autofac modules.

---
+++ Forum/App.Services/Bootloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    public static class Bootloader
+    {
+        public static void Init()
+        {
+            //Do nothing
+        }
+    }
+}
---
+++ Forum/DataAccess/Bootloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class Bootloader
+    {
+        public static void Init()
+        {
+            //Do nothing, we must be sure that the assembly is loaded
+        }
+    }
+}
