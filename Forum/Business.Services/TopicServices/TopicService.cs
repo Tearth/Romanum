38ee56fa1ff7cfb13028a88b66e79662db2d6f87
tearth <Tearth@users.noreply.github.com>
2017-08-30 19:32:30 +0200

    Add topic service

---
+++ Forum/Business.Services/TopicServices/TopicService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.TopicServices
+{
+    public class TopicService : ServiceBase, ITopicService
+    {
+        IDatabaseContext _databaseContext;
+
+        public TopicService(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+    }
+}
