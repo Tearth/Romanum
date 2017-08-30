98940d46fa51fdbc27e9264f6a57c774d469de54
tearth <Tearth@users.noreply.github.com>
2017-08-30 19:50:56 +0200

    Add TopicDTO and PostDTO

---
+++ Forum/Business.Services/DTO/Post/PostDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.DTO.Post
+{
+    public class PostDTO
+    {
+        public DateTime CreateTime { get; set; }
+        public DateTime ModifyTime { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Forum/Business.Services/DTO/Topic/TopicDTO.cs
@@ -0,0 +1,18 @@
+﻿using Business.Services.DTO.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.DTO.Topic
+{
+    public class TopicDTO
+    {
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public int UniqueNumber { get; set; }
+
+        public IEnumerable<PostDTO> Posts { get; set; }
+    }
+}
