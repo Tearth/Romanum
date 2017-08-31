1fa66a6eb35cff998e67e4bd0104c62b7bc271e3
tearth <Tearth@users.noreply.github.com>
2017-09-01 00:58:04 +0200

    Add AuthService to App.Services

---
+++ Forum/App.Services/AuthServices/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMatrix.WebData;
+
+namespace App.Services.AuthServices
+{
+    public class AuthService : ServiceBase, IAuthService
+    {
+        public AuthService()
+        {
+
+        }
+    }
+}
