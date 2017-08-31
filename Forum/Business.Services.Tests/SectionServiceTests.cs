0ff34126f2cf8e1f52abb9d268e83d0494571324
tearth <Tearth@users.noreply.github.com>
2017-08-31 14:50:12 +0200

    SectionServiceTests refactoring

--- Forum/Business.Services.Tests/SectionServiceTests.cs
+++ Forum/Business.Services.Tests/SectionServiceTests.cs
@@ -16,83 +16,88 @@ namespace Business.Services.Tests
 {
     public class SectionServiceTests
     {
-        List<Section> GetSectionsData()
+        private Mock<IDatabaseContext> GetDatabaseContextMock()
         {
-            var fixture = new Fixture();
-            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            var data = new List<Section>();
 
-            return fixture.CreateMany<Section>(3).ToList();
+            var firstSection = new Section("Section 1", "sec-1");
+            var secondSection = new Section("Section 2", "sec-2");
+            var thirdSection = new Section("Section 3", "sec-3");
+
+            data.Add(firstSection);
+            data.Add(secondSection);
+            data.Add(thirdSection);
+
+            var firstCategory = new Category("Category 1", "cat-1") { ID = 1, Section = firstSection };
+            var secondCategory = new Category("Category 2", "cat-2") { ID = 2, Section = firstSection };
+            var thirdCategory = new Category("Category 3", "cat-3") { ID = 3, Section = secondSection };
+
+            firstSection.Categories.Add(firstCategory);
+            firstSection.Categories.Add(secondCategory);
+            secondSection.Categories.Add(thirdCategory);
+
+            var firstTopic = new Topic("Topic 1", "top-1") { ID = 1, Category = firstCategory };
+            var secondTopic = new Topic("Topic 2", "top-2") { ID = 2, Category = firstCategory };
+            var thirdTopic = new Topic("Topic 3", "top-3") { ID = 3, Category = secondCategory };
+
+            firstCategory.Topics.Add(firstTopic);
+            firstCategory.Topics.Add(secondTopic);
+            secondCategory.Topics.Add(thirdTopic);
+
+            var firstPost = new Post("Content 1", new DateTime(2000, 5, 10)) { ID = 1, Topic = firstTopic };
+            var secondPost = new Post("Content 2", new DateTime(2001, 1, 2)) { ID = 2, Topic = firstTopic };
+            var thirdPost = new Post("Content 3", new DateTime(2002, 10, 12)) { ID = 3, Topic = firstTopic };
+            var fourthPost = new Post("Content 4", new DateTime(2003, 3, 27)) { ID = 4, Topic = secondTopic };
+            var fifthPost = new Post("Content 5", new DateTime(2004, 2, 1)) { ID = 5, Topic = thirdTopic };
+
+            firstTopic.Posts.Add(firstPost);
+            firstTopic.Posts.Add(secondPost);
+            firstTopic.Posts.Add(thirdPost);
+            secondTopic.Posts.Add(fourthPost);
+            thirdTopic.Posts.Add(fifthPost);
+
+            var fakeDbSet = FakeDbSetFactory.Create<Section>(data);
+            var fakeDatabaseContext = new Mock<IDatabaseContext>();
+            fakeDatabaseContext.Setup(p => p.Sections).Returns(fakeDbSet.Object);
+
+            return fakeDatabaseContext;
         }
 
         [Fact]
-        public void GetAllSetionsWithCategories_ValidData_ValidResult()
+        public void GetAllSetionsWithCategories_ValidData_ValidCategories()
         {
-            var data = GetSectionsData();
+            var databaseContextMock = GetDatabaseContextMock();
 
-            data[0].Categories.ElementAt(1)
-                   .Topics.ElementAt(2)
-                   .Posts.Add(new Post("Test content", new DateTime(3100, 1, 5)));
+            var service = new SectionService(databaseContextMock.Object);
+            var result = service.GetAllSetionsWithCategories();
 
-            var fakeDbSet = FakeDbSetFactory.Create<Section>(data);
-    
-            var databaseContext = new Mock<IDatabaseContext>();
-            databaseContext.Setup(p => p.Sections).Returns(fakeDbSet.Object);
+            Assert.Equal("Category 1", result.ElementAt(0).Categories.ElementAt(0).Name);
+            Assert.Equal("Category 2", result.ElementAt(0).Categories.ElementAt(1).Name);
+            Assert.Equal("Category 3", result.ElementAt(1).Categories.ElementAt(0).Name);
+        }
 
-            var service = new SectionService(databaseContext.Object);
+        [Fact]
+        public void GetAllSetionsWithCategories_ValidData_ValidLastPostTime()
+        {
+            var databaseContextMock = GetDatabaseContextMock();
+
+            var service = new SectionService(databaseContextMock.Object);
             var result = service.GetAllSetionsWithCategories();
             
-            Assert.Equal(new DateTime(3100, 1, 5).Date, result.ElementAt(0).Categories.ElementAt(1).LastPostTime.Date);
-        }
-
-        [Fact]
-        public void GetAllSetionsWithCategories_EmptyCategory_NoLatestPost()
-        {
-            var data = GetSectionsData();
-            data[0].Categories.ElementAt(1).Topics.Clear();
-
-            var fakeDbSet = FakeDbSetFactory.Create<Section>(data);
-
-            var databaseContext = new Mock<IDatabaseContext>();
-            databaseContext.Setup(p => p.Sections).Returns(fakeDbSet.Object);
-
-            var service = new SectionService(databaseContext.Object);
-            var result = service.GetAllSetionsWithCategories();
-
-            Assert.Equal(default(DateTime).Date, result.ElementAt(0).Categories.ElementAt(1).LastPostTime.Date);
+            Assert.Equal(new DateTime(2003, 3, 27).Date, result.ElementAt(0).Categories.ElementAt(0).LastPostTime.Date);
+            Assert.Equal(new DateTime(2004, 2, 1).Date, result.ElementAt(0).Categories.ElementAt(1).LastPostTime.Date);
+            Assert.Equal(default(DateTime), result.ElementAt(1).Categories.ElementAt(0).LastPostTime.Date);
         }
 
         [Fact]
         public void GetAllSetionsWithCategories_EmptySection_NoCategories()
         {
-            var data = GetSectionsData();
-            data[0].Categories.Clear();
+            var databaseContextMock = GetDatabaseContextMock();
 
-            var fakeDbSet = FakeDbSetFactory.Create<Section>(data);
-
-            var databaseContext = new Mock<IDatabaseContext>();
-            databaseContext.Setup(p => p.Sections).Returns(fakeDbSet.Object);
-
-            var service = new SectionService(databaseContext.Object);
+            var service = new SectionService(databaseContextMock.Object);
             var result = service.GetAllSetionsWithCategories();
 
-            Assert.Equal(0, result.ElementAt(0).Categories.Count());
-        }
-
-        [Fact]
-        public void GetAllSetionsWithCategories_NoSections_EmptyResult()
-        {
-            var data = new List<Section>();
-
-            var fakeDbSet = FakeDbSetFactory.Create<Section>(data);
-
-            var databaseContext = new Mock<IDatabaseContext>();
-            databaseContext.Setup(p => p.Sections).Returns(fakeDbSet.Object);
-
-            var service = new SectionService(databaseContext.Object);
-            var result = service.GetAllSetionsWithCategories();
-
-            Assert.Equal(0, result.Count());
+            Assert.Equal(0, result.ElementAt(2).Categories.Count());
         }
     }
 }
