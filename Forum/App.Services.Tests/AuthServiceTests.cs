0e90a62b9a2048fcc5d089653f04c5b931fbb1af
tearth <Tearth@users.noreply.github.com>
2017-09-03 22:09:00 +0200

    Add tests to AuthServiceTests For methods: UserExists, IsUserLoggedIn, LogIn

--- Forum/App.Services.Tests/AuthServiceTests.cs
+++ Forum/App.Services.Tests/AuthServiceTests.cs
@@ -51,5 +51,91 @@ namespace App.Services.Tests
 
             Assert.IsType<UserNameExistsException>(exception);
         }
+
+        [Fact]
+        public void UserExists_ExistingUserName_ReturnsTrue()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.UserExists("TestUserName")).Returns(true);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.UserExists("TestUserName");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void UserExists_NotExistingUserName_ReturnsFalse()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.UserExists("TestUserName")).Returns(false);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.UserExists("TestUserName");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsUserLoggedIn_UserIsLoggedIn_ReturnsTrue()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.IsUserLoggedIn()).Returns(true);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.IsUserLoggedIn();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsUserLoggedIn_UserIsNotLoggedIn_ReturnsTrue()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.IsUserLoggedIn()).Returns(false);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.IsUserLoggedIn();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void LogIn_ValidUserNamePassword_ReturnsTrue()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            var logInDataDTO = new LogInDTO()
+            {
+                Name = "TestUserName",
+                Password = "TestUserPassword",
+                RememberMe = true
+            };
+
+            webSecurityWrapperMock.Setup(p => p.LogIn(logInDataDTO)).Returns(true);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.LogIn(logInDataDTO);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void LogIn_InvalidUserNamePassword_ReturnsTrue()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            var logInDataDTO = new LogInDTO()
+            {
+                Name = "TestUserName",
+                Password = "TestUserPassword",
+                RememberMe = true
+            };
+
+            webSecurityWrapperMock.Setup(p => p.LogIn(logInDataDTO)).Returns(false);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var result = authService.LogIn(logInDataDTO);
+
+            Assert.True(result);
+        }
     }
 }
