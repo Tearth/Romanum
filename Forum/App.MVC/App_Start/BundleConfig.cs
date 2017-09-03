aa11b56169220e7ef6524dea5ce70415dd659b74
tearth <Tearth@users.noreply.github.com>
2017-09-03 17:37:56 +0200

    Add BundleConfig

---
+++ Forum/App.MVC/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace App.MVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/validation").Include(
+                                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new StyleBundle("~/Content/forum-style").Include(
+                                        "~/Content/style.css"));
+        }
+    }
+}
--- Forum/App.MVC/Global.asax.cs
+++ Forum/App.MVC/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace App.MVC
@@ -21,6 +22,7 @@ namespace App.MVC
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             DIConfiguration.SetDependeciesResolver();
             MapperConfig.RegisterProfiles();
         }
