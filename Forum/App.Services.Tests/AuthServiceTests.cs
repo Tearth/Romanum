c462ec61a9da3a3057ef5a382c2204bb215c3b61
tearth <Tearth@users.noreply.github.com>
2017-09-03 20:03:40 +0200

    Add two tests to AuthServiceTests Tests for CreateUser method

--- Forum/App.Services.Tests/AuthServiceTests.cs
+++ Forum/App.Services.Tests/AuthServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using App.Services.AuthServices;
+using App.Services.AuthServices.Exceptions;
+using App.Services.AuthServices.Security;
+using App.Services.DTO.Auth;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +14,42 @@ namespace App.Services.Tests
 {
     public class AuthServiceTests
     {
+        [Fact]
+        public void CreateUser_NotExistingUserName_WrapperCreateUserCalled()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.UserExists("TestUserName")).Returns(false);
 
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var userDTO = new NewUserDTO()
+            {
+                Name = "TestUserName",
+                Password = "TestPassword",
+                EMail = "user@local.domain"
+            };
+
+            authService.CreateUser(userDTO);
+
+            webSecurityWrapperMock.Verify(p => p.CreateUser(userDTO));
+        }
+
+        [Fact]
+        public void CreateUser_ExistingUserName_WrapperCreateUserCalled()
+        {
+            var webSecurityWrapperMock = new Mock<IWebSecurityWrapper>();
+            webSecurityWrapperMock.Setup(p => p.UserExists("TestUserName")).Returns(true);
+
+            var authService = new AuthService(webSecurityWrapperMock.Object);
+            var userDTO = new NewUserDTO()
+            {
+                Name = "TestUserName",
+                Password = "TestPassword",
+                EMail = "user@local.domain"
+            };
+
+            var exception = Record.Exception(() => authService.CreateUser(userDTO));
+
+            Assert.IsType<UserNameExistsException>(exception);
+        }
     }
 }
