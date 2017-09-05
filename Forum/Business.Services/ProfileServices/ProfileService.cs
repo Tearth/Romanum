413f41dcb5d607c5b3eb3193a0689709fdd2fc06
tearth <Tearth@users.noreply.github.com>
2017-09-05 14:43:27 +0200

    Add ProfileService

---
+++ Forum/Business.Services/ProfileServices/ProfileService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.ProfileServices
+{
+    public class ProfileService : ServiceBase, IProfileService
+    {
+        IDatabaseContext _databaseContext;
+
+        public ProfileService(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+    }
+}
