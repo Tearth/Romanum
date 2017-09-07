71f8c902b6dd659bcec560fd52c8ac9dd8099ed4
tearth <Tearth@users.noreply.github.com>
2017-09-07 23:53:06 +0200

    Add control panel view

---
+++ Forum/App.MVC/Controllers/ControlPanelController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.MVC.Controllers
+{
+    public class ControlPanelController : Controller
+    {
+        public ControlPanelController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
