19d23bd5e027a31d01fa42ea9a97aad6b9162947
tearth <Tearth@users.noreply.github.com>
2017-09-17 16:27:55 +0200

    Add ProfileController

---
+++ Forum/App.MVC/Controllers/ControlPanel/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Services.ProfileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.MVC.Controllers.ControlPanel
+{
+    public class ProfileController : Controller
+    {
+        IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
