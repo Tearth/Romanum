a5cb95191befa5b41f429bd5196080d7ae12253d
tearth <Tearth@users.noreply.github.com>
2017-09-17 20:49:16 +0200

    Add EMailAlreadyExistsException

---
+++ Forum/Business.Services/ProfileServices/Exceptions/EMailAlreadyExistsException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.ProfileServices.Exceptions
+{
+    [Serializable]
+    public class EMailAlreadyExistsException : Exception
+    {
+        public EMailAlreadyExistsException()
+        {
+
+        }
+
+        public EMailAlreadyExistsException(string message) : base(message)
+        {
+
+        }
+
+        public EMailAlreadyExistsException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+        protected EMailAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
