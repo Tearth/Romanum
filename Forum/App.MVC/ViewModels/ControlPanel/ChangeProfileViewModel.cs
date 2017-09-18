595c6bb8f13d90890956cb39865f822300a674c0
tearth <Tearth@users.noreply.github.com>
2017-09-18 17:46:30 +0200

    Add validation for ChangeProfileViewModel

--- Forum/App.MVC/ViewModels/ControlPanel/ChangeProfileViewModel.cs
+++ Forum/App.MVC/ViewModels/ControlPanel/ChangeProfileViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,17 @@ namespace App.MVC.ViewModels.ControlPanel
 {
     public class ChangeProfileViewModel
     {
+        [Required(ErrorMessage = "E-Mail field cannot be empty")]
+        [EmailAddress(ErrorMessage = "E-Mail invalid format")]
         public string EMail { get; set; }
+
+        [Required(ErrorMessage = "City field cannot be empty")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "About field cannot be empty")]
         public string About { get; set; }
+
+        [Required(ErrorMessage = "Footer field cannot be empty")]
         public string Footer { get; set; }
     }
 }
