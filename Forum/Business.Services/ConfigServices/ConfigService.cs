ee0bc8e5fa32b72fc26b74cda4754ed0dde69474
tearth <Tearth@users.noreply.github.com>
2017-09-19 16:28:05 +0200

    Add ConfigService class

---
+++ Forum/Business.Services/ConfigServices/ConfigService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.ConfigServices
+{
+    public class ConfigService : ServiceBase, IConfigService
+    {
+        IDatabaseContext _databaseContext;
+
+        public ConfigService(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+    }
+}
