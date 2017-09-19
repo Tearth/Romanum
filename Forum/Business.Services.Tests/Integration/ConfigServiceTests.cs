de126b8a16f909190f878c1a454e332b733e19f7
tearth <Tearth@users.noreply.github.com>
2017-09-19 18:15:59 +0200

    Add tests for GetValue method in ConfigService

---
+++ Forum/Business.Services.Tests/Integration/ConfigServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using Business.Services.ConfigServices;
+using Business.Services.Tests.Helpers.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Business.Services.Tests.Integration
+{
+    [AutoRollback]
+    public class ConfigServiceTests
+    {
+        [Fact]
+        public void GetValue_ExistingKey_ReturnsValidStringValue()
+        {
+            var testDatabaseContext = DbContextFactory.Create();
+
+            var service = new ConfigService(testDatabaseContext);
+            var result = service.GetValue<string>("Key1");
+
+            Assert.Equal("String value", result);
+        }
+
+        [Fact]
+        public void GetValue_ExistingKey_ReturnsValidBoolTrueValue()
+        {
+            var testDatabaseContext = DbContextFactory.Create();
+
+            var service = new ConfigService(testDatabaseContext);
+            var result = service.GetValue<bool>("Key2");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetValue_ExistingKey_ReturnsValidBoolFalseValue()
+        {
+            var testDatabaseContext = DbContextFactory.Create();
+
+            var service = new ConfigService(testDatabaseContext);
+            var result = service.GetValue<bool>("Key3");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GetValue_ExistingKey_ReturnsValidIntegerValue()
+        {
+            var testDatabaseContext = DbContextFactory.Create();
+
+            var service = new ConfigService(testDatabaseContext);
+            var result = service.GetValue<int>("Key4");
+
+            Assert.Equal(123, result);
+        }
+
+        [Fact]
+        public void GetValue_ExistingKey_ReturnsValidFloatValue()
+        {
+            var testDatabaseContext = DbContextFactory.Create();
+
+            var service = new ConfigService(testDatabaseContext);
+            var result = service.GetValue<float>("Key5");
+
+            Assert.Equal(10.34f, result);
+        }
+    }
+}
