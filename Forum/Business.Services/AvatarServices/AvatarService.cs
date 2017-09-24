acc56e6b440f3edcf4d9c7aeeb9d4d57fb29d32b
tearth <Tearth@users.noreply.github.com>
2017-09-25 01:27:40 +0200

    Add AvatarService

---
+++ Forum/Business.Services/AvatarServices/AvatarService.cs
@@ -0,0 +1,22 @@
+﻿using Business.Services.DTO.Avatar;
+using DataAccess.Database;
+using DataAccess.Entities;
+using DataAccess.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.AvatarServices
+{
+    public class AvatarService : ServiceBase, IAvatarService
+    {
+        IDatabaseContext _databaseContext;
+
+        public AvatarService(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+    }
+}
