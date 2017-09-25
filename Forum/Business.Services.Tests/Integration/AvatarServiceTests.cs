8e5a2a506522e55ca94fcaaed6dbd5cbe7f8bd34
tearth <Tearth@users.noreply.github.com>
2017-09-25 16:13:49 +0200

    Add AvatarServiceTests

---
+++ Forum/Business.Services.Tests/Integration/AvatarServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Business.Services.AvatarServices;
+using Business.Services.CategoryServices;
+using Business.Services.CategoryServices.Exceptions;
+using Business.Services.Tests.Helpers;
+using Business.Services.Tests.Helpers.Database;
+using DataAccess.Database;
+using DataAccess.Entities;
+using Moq;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Business.Services.Tests.Integration
+{
+    [AutoRollback]
+    public class AvatarServiceTests
+    {
+        public AvatarServiceTests()
+        {
+            DbContextFactory.Init();
+        }
+    }
+}
