0355cfeec661e88ccfa2fda7b7d68b5ed5be7e7d
tearth <Tearth@users.noreply.github.com>
2018-03-21 21:16:27 +0100

    Move statistics to the separate partial view.

---
+++ Forum/App.MVC/Controllers/Shared/StatisticsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.MVC.Controllers.Shared
+{
+    [ChildActionOnly]
+    public class StatisticsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Statistics()
+        {
+            return PartialView();
+        }
+    }
+}
