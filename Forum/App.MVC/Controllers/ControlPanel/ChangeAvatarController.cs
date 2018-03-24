f2747c67b5765408b23cce9fd1e8affcac46fb63
tearth <Tearth@users.noreply.github.com>
2018-03-24 16:32:07 +0100

    Add avatar uploading to ChangeAvatarController.

--- Forum/App.MVC/Controllers/ControlPanel/ChangeAvatarController.cs
+++ Forum/App.MVC/Controllers/ControlPanel/ChangeAvatarController.cs
@@ -2,8 +2,10 @@
 using App.MVC.ViewModels.ControlPanel;
 using App.MVC.ViewModels.ControlPanel.Enums;
 using App.Services.AuthServices;
+using App.Services.AvatarFilesServices;
 using AutoMapper;
 using Business.Services.AvatarServices;
+using Business.Services.DTO.Avatar;
 
 namespace App.MVC.Controllers.ControlPanel
 {
@@ -11,11 +13,13 @@ namespace App.MVC.Controllers.ControlPanel
     {
         private IAuthService _authService;
         private IAvatarService _avatarService;
+        private IAvatarFilesService _avatarFilesService;
 
-        public ChangeAvatarController(IAuthService authService, IAvatarService avatarService)
+        public ChangeAvatarController(IAuthService authService, IAvatarService avatarService, IAvatarFilesService avatarFilesService)
         {
             _authService = authService;
             _avatarService = avatarService;
+            _avatarFilesService = avatarFilesService;
         }
 
         [HttpGet]
@@ -31,16 +35,46 @@ namespace App.MVC.Controllers.ControlPanel
         [HttpPost]
         public ActionResult Index(ChangeAvatarViewModel viewModel)
         {
-            if (viewModel.Type == AvatarType.Internal)
+            switch (viewModel.Type)
             {
-                if (viewModel.AvatarFile == null)
+                case AvatarType.Default:
                 {
-                    ModelState.AddModelError("AvatarFile", "You have to upload file.");
-                    return View(viewModel);
+                    break;
+                }
+
+                case AvatarType.Gravatar:
+                {
+                    break;
+                }
+
+                case AvatarType.Internal:
+                {
+                    var stream = viewModel.AvatarFile.InputStream;
+                    var mimeType = viewModel.AvatarFile.ContentType;
+                    var serverPath = HttpContext.Server.MapPath("/");
+
+                    if (viewModel.AvatarFile == null)
+                    {
+                        ModelState.AddModelError("AvatarFile", "You have to upload file.");
+                        return View(viewModel);
+                    }
+
+                    if (!_avatarService.CheckIfMimeTypeIsValid(mimeType))
+                    {
+                        ModelState.AddModelError("AvatarFile", $"{mimeType} is not allowed.");
+                        return View(viewModel);
+                    }
+
+                    var pathToAvatarFile = _avatarFilesService.SaveAvatar(stream, mimeType, serverPath);
+                    var avatar = Mapper.Map<ChangedAvatarDTO>(viewModel);
+                    avatar.Source = pathToAvatarFile;
+
+                    _avatarService.SetUserAvatar(_authService.GetCurrentUser().ID, avatar);
+                    break;
                 }
             }
 
-            return View();
+            return RedirectToAction("Index");
         }
     }
 }
