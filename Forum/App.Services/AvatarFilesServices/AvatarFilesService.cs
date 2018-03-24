e433201cd0e8637cd1f2cdcd37ae9addfa619ce8
tearth <Tearth@users.noreply.github.com>
2018-03-24 16:31:47 +0100

    Add AvatarFilesService.

---
+++ Forum/App.Services/AvatarFilesServices/AvatarFilesService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.AvatarFilesServices
+{
+    /// <summary>
+    /// Represents a set of methods to manage avatar files.
+    /// </summary>
+    public class AvatarFilesService : ServiceBase, IAvatarFilesService
+    {
+        private const string AvatarsPath = "Content/Avatars/";
+
+        /// <inheritdoc />
+        public string SaveAvatar(Stream stream, string mimeType, string serverPath)
+        {
+            var extension = mimeType.Split('/').Last();
+            var path = $"{AvatarsPath}{Guid.NewGuid()}.{extension}";
+            var fullPath = $"{serverPath}{path}";
+
+            using (var file = File.Create(fullPath))
+            {
+                var buffer = new byte[1024];
+                int len;
+
+                while ((len = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    file.Write(buffer, 0, len);
+                }
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Forum/App.Services/AvatarFilesServices/IAvatarFilesService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.AvatarFilesServices
+{
+    /// <summary>
+    /// Represents an interface for avatar files services.
+    /// </summary>
+    public interface IAvatarFilesService
+    {
+        /// <summary>
+        /// Saves avatar file and generates unique path name.
+        /// </summary>
+        /// <param name="stream">The file stream to save.</param>
+        /// <param name="mimeType">The file mime type.</param>
+        /// <returns>The path to the saved avatar.</returns>
+        string SaveAvatar(Stream stream, string mimeType, string serverPath);
+    }
+}
