1b6bfb73561831ddc09cc8cac15af07301cac93e
tearth <Tearth@users.noreply.github.com>
2018-04-27 16:08:42 +0200

    Add body of post validator.

--- Forum/Business.Services/PostServices/PostValidator.cs
+++ Forum/Business.Services/PostServices/PostValidator.cs
@@ -1,14 +1,35 @@
-﻿namespace Business.Services.PostServices
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Business.Services.PostServices
 {
     /// <summary>
     /// Represents a set of methods to validate post contents.
     /// </summary>
     public class PostValidator : ServiceBase, IPostValidator
     {
+        private const int MinLength = 5;
+        private const int MaxLength = 1000;
+
+        private readonly List<string> _invalidTags = new List<string>
+        {
+            "applet",
+            "script",
+            "style",
+            "link",
+            "iframe"
+        };
+
         /// <inheritdoc />
         public bool IsValid(string content)
         {
-            return false;
+            var lowerContent = content.ToLower();
+            if (lowerContent.Length < MinLength || lowerContent.Length > MaxLength)
+            {
+                return false;
+            }
+
+            return !_invalidTags.Any(lowerContent.Contains);
         }
     }
 }
